8c2df6eb8af1299de75135af732b8cfe820d017d
MrVeGGi3 <matheusveras500@gmail.com>
2024-09-15 09:28:17 -0300

    Pré Ajustes e Organização de Pastas e Códigos

--- scripts/Characthers/boatlevel/Enemy2.gd
+++ scripts/Characthers/boatlevel/Enemy2.gd
@@ -3,23 +3,24 @@ extends Node3D
 
 @export var speed = 5
 @onready var area_3d = $Area3D
-@onrrady var auto_destruction_timer = $AutoDestructionTimer
+@onready var auto_destruction_timer = $AutoDestructionTimer
 @onready var player = get_tree.get_first_node_in_group("player")
 @onready var enemy_position = global_transform.origin
-
+var player_position
 func _ready():
-    var player_position = player.global_transform.origin
-    auto_destruction_timer.start()
+	player_position = player.global_transform.origin
+	auto_destruction_timer.start()
+	
 func _process(delta):
-    var direction = (enemy_position - player_position).normalized()
-    var movement = direction * speed * delta
-    global_transform.origin += movement
-
-    var bodies = area_3d.get_overlapping_bodies()
-    for body in bodies:
-    if body.is_in_group("player"):
-      body.damage(3)
-      queue_free()
+	var direction = (enemy_position - player_position).normalized()
+	var movement = direction * speed * delta
+	global_transform.origin += movement
+	var bodies = area_3d.get_overlapping_bodies()
+	
+	for body in bodies:
+		if body.is_in_group("player"):
+			body.damage(3)
+			queue_free()
 
 func _on_timer_timeout():
-    queue_free()
+	queue_free()
